0226fee6b54be0bf9a2b6d5b32dbfeaab4fbcf0a
MasayaOkada <masa.okd888@gmail.com>
2019-08-26 20:25:47 +0900

    add explanation

--- tutorial/src/listener.py
+++ tutorial/src/listener.py
@@ -2,9 +2,9 @@
 import rospy
 from std_msgs.msg import String
 
-def callback(message):
-	rospy.loginfo("I heard %s", message.data)
+def callback(message):						# callback関数でmessageという引数をとる
+	rospy.loginfo("I heard %s", message.data)		# 文字列を表示する
 
-rospy.init_node('listener')
-sub = rospy.Subscriber('chatter', String, callback)
-rospy.spin()
+rospy.init_node('listener')					
+sub = rospy.Subscriber('chatter', String, callback)		# std_msgs/String型のpublishをsubscribeしてcallbackを実行
+rospy.spin()							# 無限ループして受信を待ち続ける
