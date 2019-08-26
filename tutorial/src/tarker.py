532813b2102cf9778399a2817392c9c157afb666
MasayaOkada <masa.okd888@gmail.com>
2019-08-26 19:51:05 +0900

    add explanation

--- tutorial/src/tarker.py
+++ tutorial/src/tarker.py
@@ -2,12 +2,13 @@
 import rospy
 from std_msgs.msg import String
 
-rospy.init_node('talker')
-pub = rospy.Publisher('chatter', String, queue_size = 10)
-rate = rospy.Rate(10)
+rospy.init_node('talker')					# ROSの最初におこなう作業　talkerというノードができる
+pub = rospy.Publisher('chatter', String, queue_size = 10)	# chatterという名前でstd_msgs/String型のpublishをつくる
+								# queue_sizeはバッファーの格納
+rate = rospy.Rate(10)						# 10Hz
 
-while not rospy.is_shutdown():
-	hello_str = String()
-	hello_str.data = "hello world %s" % rospy.get_time()
-	pub.publish(hello_str.data)
-	rate.sleep()
+while not rospy.is_shutdown():					# Ctrl+cされるまで続ける
+	hello_str = String()					# StringのメッセージをStringクラスからつくる
+	hello_str.data = "hello world %s" % rospy.get_time()	# hello_strの内容を書き込む
+	pub.publish(hello_str.data)				# hello_strをpublish
+	rate.sleep()						# 10Hzを実現するために必要な時間をsleep
